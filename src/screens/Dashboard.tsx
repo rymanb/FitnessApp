98b08678504d50fb08512bc5add7d05debef3ffb
Ryman Barnett <boneyshark22@gmail.com>
2026-03-07 06:38:28 -0700

    first commit

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,9 @@
+import {View, Text, ScrollView} from 'react-native';
+
+export const Dashboard = () => {
+  return (
+    <View className="flex-1 bg-zinc-950">
+      <Text className="text-white">Welcome to the Dashboard</Text>
+    </View>
+  );
+};
